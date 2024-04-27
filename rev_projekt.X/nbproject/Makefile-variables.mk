b4de3c53ce5ac84afd4240c088ff95871f160f8a
oggysek <160542373+oggysek@users.noreply.github.com>
2024-04-27 15:07:23 +0200

    init

---
+++ rev_projekt.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=rev_projekt.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/rev_projekt.X.production.hex
